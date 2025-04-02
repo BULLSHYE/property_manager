4c72fab9143af658cc5ee8ce0e7ab2d88729e346
BULLSHYE <rekharahul.agarwal@gmail.com>
2025-04-02 16:49:49 +0530

    sdfsfsdfds

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,19 +5,21 @@ from .views import LandlordViewSet, PropertyViewSet, RoomViewSet, TenantViewSet,
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+from rest_framework.authentication import TokenAuthentication
 
-# Swagger Configuration
 schema_view = get_schema_view(
     openapi.Info(
-        title="Property Management API",  # Change this title to match your project name
+        title="Property Management API",
         default_version='v1',
         description="API documentation for Property Management System",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@yourdomain.com"),  # Update the contact email
+        contact=openapi.Contact(email="contact@yourdomain.com"),
         license=openapi.License(name="MIT License"),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),  # This makes the API public
+    permission_classes=(AllowAny,),  # Allow anyone to view Swagger
+    authentication_classes=[TokenAuthentication],  # Token Authentication
 )
 
 # Registering the API ViewSets with the DefaultRouter
@@ -35,5 +37,5 @@ urlpatterns = [
     path('api/property-details/<int:property_id>/<int:month>/', property_monthly_details, name="property-monthly-details"),
     
     # Swagger UI for API Documentation
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  # Swagger UI path
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
